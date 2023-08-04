6434ca0979f22998e4fe056d426f981374954fd4
Sterling Hanenkamp <sterling@speakeasyapi.dev>
2023-08-03 22:21:42 -0500

    cmd for starting/stopping grpc service

--- cmd/service/start.go
+++ cmd/service/start.go
@@ -1,8 +1,36 @@
 package service
 
-import "github.com/spf13/cobra"
+import (
+	"context"
+
+	"github.com/spf13/cobra"
+
+	s "github.com/zostay/ghost/cmd/shared"
+	"github.com/zostay/ghost/pkg/config"
+	"github.com/zostay/ghost/pkg/keeper"
+)
 
 var StartCmd = &cobra.Command{
 	Use:   "start",
 	Short: "Start the ghost service",
+	Run:   RunStartService,
+}
+
+func RunStartService(cmd *cobra.Command, args []string) {
+	c := config.Instance()
+	if c.MasterKeeper == "" {
+		s.Logger.Panic("no master keeper set")
+	}
+
+	ctx := context.Background()
+
+	kpr, err := keeper.Build(ctx, c.MasterKeeper, c)
+	if err != nil {
+		s.Logger.Panicf("failed to configure master keeper %q: %v", c.MasterKeeper, err)
+	}
+
+	err = keeper.StartServer(kpr)
+	if err != nil {
+		s.Logger.Panic(err)
+	}
 }
--- cmd/service/stop.go
+++ cmd/service/stop.go
@@ -1,8 +1,39 @@
 package service
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/spf13/cobra"
 
-var StopCmd = &cobra.Command{
-	Use:   "stop",
-	Short: "Stop the ghost service",
+	s "github.com/zostay/ghost/cmd/shared"
+	"github.com/zostay/ghost/pkg/keeper"
+)
+
+var (
+	StopCmd = &cobra.Command{
+		Use:   "stop",
+		Short: "Stop the ghost service",
+		Run:   RunStopService,
+	}
+
+	quit bool
+	kill bool
+)
+
+func init() {
+	StopCmd.Flags().BoolVar(&quit, "quit", false, "send SIGQUIT instead of SIGHUP")
+	StopCmd.Flags().BoolVar(&kill, "kill", false, "send SIGKILL instead of SIGHUP")
+}
+
+func RunStopService(cmd *cobra.Command, args []string) {
+	quickness := keeper.StopGraceful
+	switch {
+	case kill:
+		quickness = keeper.StopNow
+	case quit:
+		quickness = keeper.StopQuick
+	}
+
+	err := keeper.StopServer(quickness)
+	if err != nil {
+		s.Logger.Panic(err)
+	}
 }
